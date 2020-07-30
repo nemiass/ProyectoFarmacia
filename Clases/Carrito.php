0e53b49c428a3f18f5b74655b486780718abe80c
nemiass <2017210145@udh.edu.pe>
2020-07-30 07:31:12 -0500

    corrigiendo

---
+++ Clases/Carrito.php
@@ -0,0 +1,21 @@
+<?php
+namespace Clases;
+class Carrito
+{
+    private $cantidad;
+
+    public function Agregar()
+    {
+        // TODO
+    }
+
+    public function Eliminar()
+    {
+        // TODO
+    }
+    
+    public function Listar()
+    {
+        // TODO
+    }
+}
