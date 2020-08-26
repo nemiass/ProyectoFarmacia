e3393318eabda4493fb82829f9bcd33bbc9995f5
nemiass <2017210145@udh.edu.pe>
2020-08-26 07:00:40 -0500

    no validado

--- app/controller/PedidoController.php
+++ app/controller/PedidoController.php
@@ -8,81 +8,70 @@ use app\Pedido_producto;
 
 class PedidoController
 {
-
-    function agregarPedido()
+    function registrarPedido($id_cliente)
     {
-    }
-
-    function eliminarPedido()
-    {
-    }
-
-    function registrarPedido($id_cliente){
-     
-        if(isset($_POST['enviar'])){
-               $fecha=$_POST['fecha'];
-               $fecha_entrega=date("Y-m-d");
-               $direccion=$_POST['direccion'];
-               $id_cliente=$id_cliente;
-            $pedido=new Pedido($fecha,$fecha_entrega,$direccion,$id_cliente);
-            $ped=$pedido->registrarPedido();
-           return $ped;
-         
+        if (isset($_POST['enviar'])) {
+            $fecha = $_POST['fecha'];
+            $fecha_entrega = date("Y-m-d");
+            $direccion = $_POST['direccion'];
+            $id_cliente = $id_cliente;
+            $pedido = new Pedido($fecha, $fecha_entrega, $direccion, $id_cliente);
+            $ped = $pedido->registrarPedido();
+            return $ped;
         }
-      
     }
 
-    function registrarPedidoProducto($cantidad,$id_prod,$id_pedido,$id_producto){
-        if(isset($_POST['enviar'])){
-            $ped_pro=new Pedido_producto($cantidad,$id_prod,$id_pedido,$id_producto);
+    function registrarPedidoProducto($cantidad, $id_prod, $id_pedido, $id_producto)
+    {
+        if (isset($_POST['enviar'])) {
+            $ped_pro = new Pedido_producto($cantidad, $id_prod, $id_pedido, $id_producto);
             $ped_pro->registrarPedido_producto();
-          
         }
     }
     function listarPedidosAtendidos()
     {
-        $pedido=Pedido::ListarPedidosAtendidos();
+        $pedido = Pedido::ListarPedidosAtendidos();
         return $pedido;
     }
 
-    function nuevoPedido(){
-        $_SESSION["Productos"]=null;
-       
+    function nuevoPedido()
+    {
+        $_SESSION["Productos"] = null;
     }
 
     function listarPedidosAtendidosempleado($id)
     {
-        $pedido=Pedido::PedidoRealizadoEmpleado($id);
+        $pedido = Pedido::PedidoRealizadoEmpleado($id);
         return $pedido;
     }
 
     function listarpedidosaAtender()
     {
-        $pedido=Pedido::PedidoaAtender();
+        $pedido = Pedido::PedidoaAtender();
         return $pedido;
     }
 
-    function registrarEmpleado_pedido(){
-        if(isset($_POST['enviar'])){
-           $id_pedido= $_POST['id_pedido'];
-           $id_empleado= $_POST['id_empleado'];
-          $empleado_pedido=new Empleado_pedido($id_empleado,$id_pedido);
-          $empleado_pedido->registrarEmpleado_pedido();
-          Pedido::actualizarEstado($id_pedido);
+    function registrarEmpleado_pedido()
+    {
+        if (isset($_POST['enviar'])) {
+            $id_pedido = $_POST['id_pedido'];
+            $id_empleado = $_POST['id_empleado'];
+            $empleado_pedido = new Empleado_pedido($id_empleado, $id_pedido);
+            $empleado_pedido->registrarEmpleado_pedido();
+            Pedido::actualizarEstado($id_pedido);
         }
-
-    
     }
 
-    function detallesaAtender($id_cliente,$id_pedido){
-        $pedido=Pedido::detallesaAtender($id_cliente,$id_pedido);
+    function detallesaAtender($id_cliente, $id_pedido)
+    {
+        $pedido = Pedido::detallesaAtender($id_cliente, $id_pedido);
         return $pedido;
-   }
+    }
 
-   
-   function PedidosAtendidos($id_empleado){
-    $pedido=Pedido::pedidos_atendido($id_empleado);
-    return $pedido;
-}
 
+    function PedidosAtendidos($id_empleado)
+    {
+        $pedido = Pedido::pedidos_atendido($id_empleado);
+        return $pedido;
+    }
 }
--- view/carrito.php
+++ view/carrito.php
@@ -1,19 +1,35 @@
 <!-- Header -->
 <?php
- 
+
 use app\controller\ClienteController;
 use app\controller\PedidoController;
 
-$ccontroller = new ClienteController;
-$pcontroller=new PedidoController;
-$productoss=$_SESSION["Productos"];
-$cliente = $ccontroller->traerCliente($_SESSION['dni']);
-$id_cliente=$cliente['id_cliente'];
+if (isset($_POST['enviar'])) {
+  if (isset($_SESSION["estado"])) {
+    $ccontroller = new ClienteController;
+    $pcontroller = new PedidoController;
 
-//session_start();
+    $cliente = $ccontroller->traerCliente($_SESSION['dni']);
+    $id_cliente = $cliente['id_cliente'];
+    $pedido = $pcontroller->registrarPedido($id_cliente);
+    $productoss = $_SESSION["Productos"];
+    $id_pedido = $pedido[0]['id_pedido'];
+
+    foreach ($productoss as $p) {
+      $id = $p["id"];
+      $nombre = $p["nombre"];
+      $precio = $p["precio"];
+      $cantidad = $p["cantidad"];
+      $subtotal = $p["subtotal"];
+      $pcontroller->registrarPedidoProducto($cantidad, $id, $id_pedido, $id);
+    }
+  } else {
+    $dato = "Registrese para poder realizar su pedido correctamente!!";
+  }
+}
+
+
 include "layouts/headerCliente.php";
-//$pedido=$pcontroller->registrarPedido($cliente['id_cliente']);
-
 ?>
 <!--/. Header -->
 
@@ -27,7 +43,7 @@ include "layouts/headerCliente.php";
         <div class="row">
           <div class="col-10">
             <h1></h1>
-                      </div>
+          </div>
           <!--<div class="col-2 mt-3">
                       <?php
                       /**if (isset($_SESSION["total"])) {
@@ -42,42 +58,40 @@ include "layouts/headerCliente.php";
 
         <div class="row">
           <div class="col-12">
-        
+
             <table class="table table-striped" id="lista-pedidos">
               <thead>
                 <tr>
+                  <?php if (isset($dato)) : ?>
+                    <div class='alert alert-warning' role='alert'><?= $dato ?></div>
+                  <?php endif; ?>
                   <th>Producto</th>
                   <th>Precio</th>
                   <th>Cantidad</th>
                   <th>Sub Total</th>
                   <th>&nbsp</th>
-                  <?= var_dump($productoss) ?>
                 </tr>
               </thead>
 
               <tbody>
                 <?php
-                //session_start();
-                $pedido=$pcontroller->registrarPedido($id_cliente);
-             
-               $id_pedido=$pedido[0]['id_pedido'];
                 if (!empty($_SESSION["Productos"])) :
+                  $productoss = $_SESSION["Productos"];
                   foreach ($productoss as $p) :
                     $id = $p["id"];
                     $nombre = $p["nombre"];
                     $precio = $p["precio"];
                     $cantidad = $p["cantidad"];
                     $subtotal = $p["subtotal"];
-                    $pcontroller->registrarPedidoProducto($cantidad,$id,$id_pedido,$id);
+
                 ?>
-             
-                    <tr id="<?= $id ?>">
+                    <tr id="<?= $id ?>A">
                       <td><?= $nombre ?></td>
                       <td>S/<?= number_format($precio, 2, ".", ",") ?></td>
                       <td><input class='text-center' id="<?= $id ?>" disabled style='width:17%;' type="text" value="<?= $cantidad ?>"></td>
                       <td>S/<?= number_format($subtotal, 2, ".", ",") ?></td>
                       <td>
-                        <a href="index.php?p=carrito"><button value="eliminar" type="button" class="btn btn-block btn-danger" name="eliminar" onclick="eliminar(<?= $id ?>, this.value)">Eliminar</button></a>
+                        <button value="eliminar" type="button" class="btn btn-block btn-danger" name="eliminar" onclick="eliminar(<?= $id ?>, this.value)">Eliminar</button>
                       </td>
                     </tr>
                   <?php
@@ -85,47 +99,26 @@ include "layouts/headerCliente.php";
                 else : ?>
                   <div class='alert alert-warning' role='alert'>Tu carrito está vacío!</div>
                 <?php endif; ?>
-                
+
               </tbody>
             </table>
-       
+
           </div>
         </div>
-        <form  role="form" method="POST" >          
-                            <label for="exampleInputEmail1">direccion de entrega</label>
-                            <input type="texto" class="form-control"  id="exampleInputEmail1"  name="direccion">
-                      
-              
-       
-
-                            <label for="exampleInputEmail1">Fecha de entrega </label>
-                            <input type="date"  placeholder="dd-mm-yyyy"class="form-control"  id="exampleInputEmail1"  name="fecha">
-                      <br><br>
-                   
-        <div class="row">
-      
-        <?php if(!isset($_POST['enviar'])): ?>
+        <form role="form" method="POST">
+          <label for="exampleInputEmail1">direccion de entrega</label>
+          <input type="texto" class="form-control" id="exampleInputEmail1" name="direccion">
+          <label for="exampleInputEmail1">Fecha de entrega </label>
+          <input type="date" placeholder="dd-mm-yyyy" class="form-control" id="exampleInputEmail1" name="fecha">
+          <br><br>
           <button type="submit" class="btn btn-block btn-primary btn-lg" name="enviar">Realizar Pedido</button>
-     
+          <div class="row">
+          </div>
 
-
-
- 
- 
-
-     
-        </div>
-        
       </div>
       </form>
       <!-- /.card-body -->
-        <?php else: ?>
-        <?php  $_SESSION["Productos"]=null;
-                      $_SESSION["cantidad"]=null;  ?>
-      <div class="row">
-  <a href=" index.php?p=productos"><button type="button" class="btn btn-block btn-primary btn-lg" name="enviar">Pedido Exitoso</button></a></div>
-    </div>
-    <?php  endif ?>
+
   </section>
   <!-- /.content -->
 
